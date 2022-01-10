0e288a743b115bb1618025e3657c0e2d4d08a282
Fernanda Akhsanuddin Almas <Akhsan.fr@gmail.com>
2022-01-10 08:07:51 +0700

    apdet app provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,28 +1,20 @@
 <?php
-
 namespace App\Providers;
-
-use Illuminate\Support\ServiceProvider;
-
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-}
+use Illuminate\Support\ServiceProvider;//import this
+use Illuminate\Support\Facades\Schema;class AppServiceProvider extends ServiceProvider{
+  /**
+  * Register any application services.
+  *
+  * @return void
+  */  public function register(){
+     //add this
+     Schema::defaultStringLength(191);
+  }
+  /**
+  * Bootstrap any application services.
+  *
+  * @return void
+  */
+   public function boot(){
+      //
+   }}
